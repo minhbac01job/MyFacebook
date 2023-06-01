037a3b9d360a581b393df440b9112b3fe5b03ebd
minhbac01job <minhbac01.job@gmail.com>
2023-06-01 13:47:26 +0700

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import Top from './components/top/top';
+
+function App() {
+    return (
+        <div className="App flex justify-center">
+            <Top />
+        </div>
+    );
+}
+
+export default App;
